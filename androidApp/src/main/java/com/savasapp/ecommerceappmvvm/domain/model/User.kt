ea6759b8fe6b48c1a3a8d6f6b74dd6a34a18aac8
spravia <sabaspravia@hotmail.com>
2023-06-09 10:47:09 -0600

    test

--- androidApp/src/main/java/com/savasapp/ecommerceappmvvm/domain/model/User.kt
+++ androidApp/src/main/java/com/savasapp/ecommerceappmvvm/domain/model/User.kt
@@ -4,12 +4,12 @@ import com.google.gson.Gson
 import com.google.gson.annotations.SerializedName
 
 data class User(
-    @SerializedName("id") val id: String? = null,
+    @SerializedName("id") val id: Int? = null,
     @SerializedName("nombres") val name : String,
     @SerializedName("apellidos") val lastname : String,
-    @SerializedName("email") val email : String,
+    //@SerializedName("email") val email : String,
     @SerializedName("telefono") val phone: String,
-    @SerializedName("password") val password : String,
+    //@SerializedName("password") val password : String,
     @SerializedName("roles") val roles : String
 ){
     fun toJson(): String = Gson().toJson(this)
