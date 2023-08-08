864ca8cc0485e9eb4e562b0df1714fc16dfab439
Savas Pravia Vega <sabaspravia@hotmail.com>
2023-08-08 11:38:01 -0600

    test

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview:1.4.3")
     implementation("androidx.compose.foundation:foundation:1.4.3")
     implementation("androidx.compose.material:material:1.4.3")
+    implementation("androidx.compose.material3:material3:1.1.1")
     implementation("androidx.activity:activity-compose:1.7.1")
 
     implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
